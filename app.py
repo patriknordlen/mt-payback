b3c20cc1503cb95f0610144ed59892f7149bd80f
Patrik Nordlén <patriki@gmail.com>
2022-09-29 11:58:16 +0200

    wip

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template, request, make_response
+import base64
 import datetime
 import requests
 import json
@@ -17,12 +18,20 @@ stations = {
 @app.route("/", methods=["GET"])
 def index():
     resp = make_response(
-        render_template("main.html", today=datetime.date.today(), persons=get_customers().keys(), **request.cookies)
+        render_template(
+            "main.html",
+            today=datetime.date.today(),
+            persons=get_customers(request.cookies.get("ticketholders")),
+            **request.cookies,
+        )
     )
 
     if (
         request.cookies.get("expirydate") == None
-        or datetime.datetime.strptime(request.cookies.get("expirydate"), "%Y-%m-%d").date() < datetime.date.today()
+        or datetime.datetime.strptime(
+            request.cookies.get("expirydate"), "%Y-%m-%d"
+        ).date()
+        < datetime.date.today()
     ):
         resp.delete_cookie("expirydate")
         resp.delete_cookie("ticketholder")
@@ -33,18 +42,32 @@ def index():
 
 @app.route("/api/submit", methods=["POST"])
 def submit():
-    r = requests.post(
-        "https://evf-regionsormland.preciocloudapp.net/api/Claims",
-        json=create_request_body(
+    ticketholders = json.loads(
+        base64.b64decode(request.cookies.get("ticketholders")).decode("latin-1")
+    )
+
+    print(
+        create_request_body(
             request.form.get("ticket"),
             request.form.get("from"),
             request.form.get("to"),
             request.form.get("departure"),
-            request.form.get("ticketholder"),
+            ticketholders[request.form.get("ticketholder")],
         )
     )
 
-    if r:
+    # r = requests.post(
+    #     "https://evf-regionsormland.preciocloudapp.net/api/Claims",
+    #     json=create_request_body(
+    #         request.form.get("ticket"),
+    #         request.form.get("from"),
+    #         request.form.get("to"),
+    #         request.form.get("departure"),
+    #         request.form.get("ticketholder"),
+    #     )
+    # )
+
+    if True:
         resp = make_response("Request submitted!")
         resp.set_cookie("ticketholder", request.form.get("ticketholder"))
         resp.set_cookie("expirydate", request.form.get("expirydate"))
@@ -59,7 +82,7 @@ def submit():
 def get_departures(station, date):
     r = requests.get(
         "https://evf-regionsormland.preciocloudapp.net/api/TrainStations/GetDepartureTimeList",
-        params={"stationId": station, "departureDate": date}
+        params={"stationId": station, "departureDate": date},
     )
 
     return r.text
@@ -69,12 +92,20 @@ def get_departures(station, date):
 def get_arrival_stations(station):
     arrival_stations = {"U": ["Cst", "Srv"], "Cst": ["U"], "Srv": ["U"]}
 
-    return {"stations": [{"name": x, "longname": stations[x]["name"]} for x in arrival_stations[station]]}
+    return {
+        "stations": [
+            {"name": x, "longname": stations[x]["name"]}
+            for x in arrival_stations[station]
+        ]
+    }
 
 
-def get_customers():
-    with open("ticketholders.json") as f:
-        customers = json.load(f)
+def get_customers(ticketholders):
+    if ticketholders:
+        print(ticketholders)
+        customers = json.loads(base64.b64decode(ticketholders).decode("latin-1"))
+    else:
+        customers = {}
 
     return customers
 
@@ -86,7 +117,7 @@ def get_customer_details(name):
 def create_request_body(ticket, dep_station, arr_station, departure, name):
     return {
         "id": "00000000-0000-0000-0000-000000000000",
-        "customer": get_customer_details(name),
+        "customer": name,
         "ticketNumber": ticket,
         "ticketType": 1,
         "departureStationId": stations[dep_station]["id"],
